91bb9f9e22f3fe9f0a109325907b69b88901170a
Nicolas Derumigny <nderumigny@gmail.com>
2019-05-30 23:28:16 +0200

    Initial commit

---
+++ bench.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+	NB_THREADS=$((1+`lscpu | grep "^CPU(s):" | sed -E "s/.*([0-9]+).*/\1/g"`))
+else
+	NB_THREADS=$1
+fi
+
+if [ -z linux ]; then
+	git clone --branch v5.1 --depth 1 https://github.com/torvalds/linux.git
+fi
+
+cd linux
+test | make oldconfig > /dev/null
+make clean 1>&2 >/dev/null
+time make -j$NB_THREADS
+cd ..
