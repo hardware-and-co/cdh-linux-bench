80c28bb4d3d58dbcd4acc9a498ecef63d52de24d
Nicolas Derumigny <nd@comptoir-hardware.com>
2019-06-18 19:21:27 +0200

    Ability to launch from any location

--- bench.sh
+++ bench.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
+DIR=`dirname $0`
 if [ "$1" == "" ]; then
-	NB_THREADS=$((1+`lscpu | grep "^CPU(s):" | sed -E "s/.* ([0-9]+)/\1/g"`))
+	NB_THREADS=$((`lscpu | grep "^CPU(s):" | sed -E "s/.* ([0-9]+)/\1/g"`))
 else
 	NB_THREADS=$1
 fi
 
+cd $DIR
 if [ ! -d linux ]; then
 	git clone --branch v5.1 --depth 1 https://github.com/torvalds/linux.git
 fi
@@ -13,4 +16,3 @@ cd linux
 yes "" | make oldconfig 2>&1 >/dev/null
 make clean 2>&1 >/dev/null
 time make -j$NB_THREADS
-cd ..
