86d5b825698697ccaef5e2b597c58d83b512ebc6
Nicolas Derumigny <nderumigny@gmail.com>
2020-09-10 18:36:20 +0200

    resilience to filesystem links

--- bench.sh
+++ bench.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-DIR=`dirname $0`
+SELF=`readlink -f $0`
+DIR=`dirname $SELF`
 if [ "$1" == "" ]; then
 	NB_THREADS=$((`lscpu | grep "^CPU(s):" | sed -E "s/.* ([0-9]+)/\1/g"`))
 else
@@ -11,8 +12,10 @@ cd $DIR
 if [ ! -d linux ]; then
 	git clone --branch v5.8 --depth 1 https://github.com/torvalds/linux.git
 fi
+cd -
 
-cd linux
+cd $DIR/linux
 yes "" | make oldconfig 2>&1 >/dev/null
 make clean 2>&1 >/dev/null
 time make -j$NB_THREADS
+cd -
